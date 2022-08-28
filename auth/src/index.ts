698c4ef5cdc3dd86cc889ae4b14f0c13e9f2a34a
Muhammad5943 <muhammad.aji.putra.mp98@gmail.com>
2022-08-28 17:07:57 +0700

    feat: add first test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,5 @@
-import express from 'express';
-
-const app = express();
+import app from './app';
 const PORT = 3000;
-
-app.get('*', (req, res) => {
-    res.status(200).json({
-        status: 200,
-        message: 'Hello world'
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Listening on port http//localhost:${PORT}`);
